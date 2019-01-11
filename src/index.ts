e76bee51f0d3218f0d720644cb7cd21aac481e9b
Kevin Liu <kevin@potatofrom.space>
2019-01-11 11:10:31 -0500

    Add cron and express boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import * as lw from "./LaserWatchApi";
+import cron from "node-cron";
+import express from "express";
 
-(async () => {
-  const cacheId = await lw.generateReport();
-  console.log("Generated report with cache ID", cacheId);
-  console.log(await lw.generateCsv(cacheId));
-})();
+// (async () => {
+//   const cacheId = await lw.generateReport();
+//   console.log("Generated report with cache ID", cacheId);
+//   console.log(await lw.generateCsv(cacheId));
+// })();
+
+cron.schedule("* * * * *", () => {
+  console.log("Running every minute.");
+});
+
+const app = express();
+app.get("/", (_, res) => res.send("HELLO WORLD!"));
+
+app.listen(4000, () => console.log("API server running on port 4000"));
