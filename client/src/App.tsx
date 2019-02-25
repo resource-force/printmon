689a66466b905427e66dfba84f7a6725e9bed29e
Kevin Liu <kevin@potatofrom.space>
2019-02-25 15:58:22 -0500

    Add client file framework

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
 
 class App extends Component {
   render() {
-    return (<></>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
