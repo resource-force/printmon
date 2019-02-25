947a88e55d2d1fb89e2f1b1d8b6e2f7657760258
Kevin Liu <kevin@potatofrom.space>
2019-02-25 16:01:22 -0500

    client: add prerendering support

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import "react-app-polyfill/ie9"; // For IE 9-11 support (https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill)
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-snapshot";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
