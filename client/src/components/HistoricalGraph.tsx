5185bb0569d25408f1b1c1fae9e0623eb22fc0c6
Kevin Liu <kevin@potatofrom.space>
2019-02-27 13:44:11 -0500

    Add some responsive design

--- client/src/components/HistoricalGraph.tsx
+++ client/src/components/HistoricalGraph.tsx
@@ -1,12 +1,45 @@
-import React, { useEffect } from "react";
-import { VictoryPie } from "victory";
+import React, { useEffect, useState } from "react";
+import {
+  LineChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Line
+} from "recharts";
 
-export default () => {
-  useEffect(() => {
-    (async () => {
-      const values = await fetch("/api/historical?startDate=2017-01-01");
-      console.log(await values.json());
-    })();
-  }, []);
-  return <VictoryPie />;
+export default ({ data }: { data: { [date: string]: number } }) => {
+  let totals = [];
+  let total = 0;
+  for (const date in data) {
+    totals.push({
+      date,
+      total: data[date]
+    });
+    total += data[date];
+  }
+  return (
+    <>
+      <LineChart
+        width={1000}
+        height={700}
+        data={totals}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+      </LineChart>
+      <h2>Total: {total}</h2>
+    </>
+  );
 };
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HistoricalGraph from "../components/HistoricalGraph";
+import moment from "moment";
 
-export default () => (
-  <>
-    <h1>Victory!</h1>
-    <HistoricalGraph />
-  </>
-);
+export default () => {
+  const [startDate, setStartDate] = useState(moment("2018-07-01"));
+  const [endDate, setEndDate] = useState(moment());
+  const [dailyTotals, setDailyTotals] = useState<
+    { [date: string]: number } | undefined
+  >(undefined);
+
+  async function updateTotals() {
+    const values = await fetch(
+      `/api/historical?startDate=${startDate.format(
+        "YYYY-MM-DD"
+      )}&endDate=${endDate.format("YYYY-MM-DD")}`
+    );
+    setDailyTotals(await values.json());
+  }
+
+  useEffect(() => {
+    updateTotals();
+  }, []);
+
+  return (
+    <>
+      <h1>
+        Printing from {startDate.toLocaleString()} to {endDate.toLocaleString()}
+      </h1>
+      {dailyTotals !== undefined && <HistoricalGraph data={dailyTotals} />}
+      <input
+        type="date"
+        value={startDate.format("YYYY-MM-DD")}
+        onChange={e => setStartDate(moment(e.target.value))}
+        onBlur={updateTotals}
+      />{" "}
+      to
+      <input
+        type="date"
+        value={endDate.format("YYYY-MM-DD")}
+        onChange={e => setEndDate(moment(e.target.value))}
+        onBlur={updateTotals}
+      />
+    </>
+  );
+};
