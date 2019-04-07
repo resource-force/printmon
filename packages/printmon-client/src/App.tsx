5ffeeaf9fa691ced0a006c853565f0481b9b360f
Kevin Liu <kevin@potatofrom.space>
2019-04-07 14:35:20 -0400

    Add basic about page

--- packages/printmon-client/src/App.tsx
+++ packages/printmon-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/about" exact component={About} />
         </Switch>
       </BrowserRouter>
     );
---
+++ packages/printmon-client/src/pages/About.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <>It's printmon!</>;
+}
