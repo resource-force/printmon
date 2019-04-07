58a922226f0f2c2b4000d9e4650011f3ed6643fa
Kevin Liu <kevin@potatofrom.space>
2019-04-07 14:01:38 -0400

    Updates

---
+++ precache-manifest.3887f7318fcc2635a93e3c2f12ff6d2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2218cb25c9e8cad16f01",
+    "url": "/printmon/static/js/runtime~main.2218cb25.js"
+  },
+  {
+    "revision": "2fe57899826730d9b812",
+    "url": "/printmon/static/js/main.2fe57899.chunk.js"
+  },
+  {
+    "revision": "8fd3212b778de6e1715c",
+    "url": "/printmon/static/js/2.8fd3212b.chunk.js"
+  },
+  {
+    "revision": "2fe57899826730d9b812",
+    "url": "/printmon/static/css/main.e7d6913c.chunk.css"
+  },
+  {
+    "revision": "260f6d3db37dd60abdef8f32ba60e308",
+    "url": "/printmon/index.html"
+  }
+];
