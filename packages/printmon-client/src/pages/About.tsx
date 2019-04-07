6a582d56531989eda27b9b73494608514b045c7f
Kevin Liu <kevin@potatofrom.space>
2019-04-07 15:32:29 -0400

    Add more text to about page

--- packages/printmon-client/src/pages/About.tsx
+++ packages/printmon-client/src/pages/About.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function About() {
-  return <>It's printmon!</>;
+  return (
+    <>
+      <h1>About AB Printing Monitor</h1>
+
+      <p>
+        AB Printing Monitor tracks the daily printing amounts of
+        Acton-Boxborough Regional High School.
+      </p>
+
+      <h2>Copyright</h2>
+
+      <p>
+        &copy; 2019 Kevin Liu and Kunal Sharda. This program is free software:
+        you can redistribute it and/or modify it under the terms of the{" "}
+        <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">
+          GNU Affero General Public License
+        </a>{" "}
+        as published by the Free Software Foundation, either version 3 of the
+        License, or (at your option) any later version.
+      </p>
+    </>
+  );
 }
