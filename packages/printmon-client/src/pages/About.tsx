21ffecb55fd3907a49145d89c31817feacda603b
Kevin Liu <kevin@potatofrom.space>
2019-05-27 15:22:38 -0400

    Update about page

--- packages/printmon-client/src/pages/About.tsx
+++ packages/printmon-client/src/pages/About.tsx
@@ -3,23 +3,54 @@ import React from "react";
 export default function About() {
   return (
     <>
-      <h1>About AB Printing Monitor</h1>
+      <h1>About AB Printing</h1>
 
       <p>
-        AB Printing Monitor tracks the daily printing amounts of
+        AB Printing tracks the daily printing and copying amounts of
         Acton-Boxborough Regional High School.
       </p>
+
+      <h2>How do you track printing?</h2>
+
+      <p>
+        The school EdTech department has, for the past several years, tracked
+        printing and copying activity anonymously using a software called
+        LaserWatch. This data was pulled in and analyzed to get the real-time
+        statistics as shown on the home page.
+      </p>
+
+      <h2>Image Credits</h2>
+
+      <ul>
+        <li>
+          Tree Image -{" "}
+          <a href="http://pluspng.com/png-124472.html">Tree Clipart PNG</a>.
+        </li>
+        <li>
+          James Stanier -{" "}
+          <a href="https://creativecommons.org/licenses/by-nc-sa/2.0/">
+            A field in Falmer, Sussex
+          </a>
+          . Licensed under CC BY-NC-SA 2.0.
+        </li>
+      </ul>
 
       <h2>Copyright</h2>
 
       <p>
-        &copy; 2019 Kevin Liu and Kunal Sharda. This program is free software:
-        you can redistribute it and/or modify it under the terms of the{" "}
+        &copy; 2019 Kevin Liu, Aneesh Edara and Kunal Sharda. This program is
+        free software: you can redistribute it and/or modify it under the terms
+        of the{" "}
         <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">
           GNU Affero General Public License
         </a>{" "}
         as published by the Free Software Foundation, either version 3 of the
         License, or (at your option) any later version.
+      </p>
+
+      <p>
+        See the source code{" "}
+        <a href="https://github.com/resource-force/printmon">here</a>.
       </p>
     </>
   );
