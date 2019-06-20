d1072daf0e533d83a6b4ab0ff1bf25461055ee81
Kevin Liu <kevin@potatofrom.space>
2019-06-20 19:27:29 -0400

    Force decimal point to appear

--- packages/printmon-client/src/components/RollupCounter.tsx
+++ packages/printmon-client/src/components/RollupCounter.tsx
@@ -3,15 +3,20 @@ import React, { useState, useEffect } from "react";
 //@ts-ignore
 import useOdometer from "use-odometer";
 import "odometer/themes/odometer-theme-car.css";
+import "./RollupCounter.scss";
 
 export default function RollupCounter({ num }: { num: number }) {
+  let displayNum = +num.toFixed(1) + 0.01;
+
   const targetRef = React.useRef(null);
+
   // Roll up the number from zero on initial mount, have it update as num
   // changes as well.
   const [cur, setCur] = useState(0);
   useEffect(() => {
-    setCur(num);
-  }, [num]);
+    setCur(displayNum);
+  }, [displayNum]);
+
   useOdometer(targetRef, cur);
 
   return <span ref={targetRef} />;
